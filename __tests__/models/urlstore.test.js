4d2245befa020bed6e845b1f06b5e585eb606a80
Aakash Verma <aku06_ran@yahoo.co.in>
2018-03-06 12:29:59 +0530

    urlstore.test.js - Added Model validations test

--- __tests__/models/urlstore.test.js
+++ __tests__/models/urlstore.test.js
@@ -17,3 +17,21 @@ describe('Testing URLPairs table', () => {
     }).catch(err => console.log(err));
   });
 });
+
+describe('Testing length validation of shortUrl', () => {
+  it('Testing shorturl of length 7', (done) => {
+    Models.URLPairs.create({ longURL: 'www.example.com', shortURL: 'abcdefg' })
+      .catch((err) => { expect(err).not.toBe(null); done(); });
+  });
+  it('Testing shortURL of length 5', (done) => {
+    Models.URLPairs.create({ longURL: 'www.example.com', shortURL: 'abcde' })
+      .catch((err) => { expect(err).not.toBe(null); done(); });
+  });
+  it('Testing shortURL of length 6', (done) => {
+    Models.URLPairs.create({ longURL: 'www.example.com', shortURL: 'abcdef' })
+      .then((value) => {
+        expect(value.dataValues).not.toBe(null);
+        done();
+      });
+  });
+});
