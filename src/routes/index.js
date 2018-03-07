e48c6eb39acbdcabea9fdf8261d9582405f15a8e
Aakash Verma <aku06_ran@yahoo.co.in>
2018-03-07 17:27:36 +0530

    Added ping endpoint for load test

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,4 +1,5 @@
 const getShortUrl = require('./getShortUrl');
 const getLongUrl = require('./getLongUrl');
+const ping = require('./ping');
 
-module.exports = [].concat(getShortUrl, getLongUrl);
+module.exports = [].concat(getShortUrl, getLongUrl, ping);
