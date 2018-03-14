f9292bb09ed445c9bc163965417b0fbccadf95e8
Ubuntu <ubuntu@ip-172-31-55-222.ec2.internal>
2018-03-14 06:26:57 +0000

    Modified seeder for production; needs to be changed for dev or test use

--- seeders/20180305043529-feedLongURLs.js
+++ seeders/20180305043529-feedLongURLs.js
@@ -1,7 +1,7 @@
 const md5 = require('md5-base64');
 const redis = require('redis');
 
-const client = redis.createClient(); // creates a new client
+const client = redis.createClient(6379, '172.31.62.11'); // creates a new client
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -12,10 +12,10 @@ module.exports = {
     let shortURLInstance = '';
     let urlObj = {};
     let md5Instance = '';
-    // client.on('connect', () => {
-    //   console.log('connected');
-    // });
-    for (let i = 0; i < 1000000; i += 1) {
+     client.on('connect', () => {
+       console.log('connected');
+    });
+    for (let i = 0; i < 100000; i += 1) {
       longURLInstance = `${longURLtemplate}${i}`;
       md5Instance = md5(longURLInstance);
       shortURLInstance = md5Instance.substr(0, 6).replace(/\//g, '_');
@@ -24,7 +24,7 @@ module.exports = {
         shortURLInstance = md5Instance.substr(incrSubs, 6).replace(/\//g, '_');
         incrSubs += 1;
       }
-      // client.set(shortURLInstance, longURLInstance);
+      client.set(shortURLInstance, longURLInstance);
       const time = new Date();
       shortURLSet.add(shortURLInstance);
       urlObj = {
